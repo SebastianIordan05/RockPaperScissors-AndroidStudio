f5d8c1de30aecc7cf2ce761e7055b060b52a5144
SebastianIordan05 <sebastian.iordan@edu.iisleviponti.it>
2024-02-24 21:39:50 +0100

    added imgs and buttons

--- app/src/main/java/com/rps/rockpaperscissorsiordan/MainActivity.kt
+++ app/src/main/java/com/rps/rockpaperscissorsiordan/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.withConsumedWindowInsets
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,8 +16,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import com.rps.rockpaperscissorsiordan.ui.theme.RockPaperScissorsIordanTheme
 
@@ -43,24 +48,35 @@ fun Layout() {
     Column (
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        modifier = Modifier.padding(10.dp)
+        modifier = Modifier.padding(horizontal = 10.dp, vertical = 10.dp)
     ) {
-        Text(
-            text = stringResource(id = R.string.title)
+        Text (
+            text = stringResource(id = R.string.title),
+            fontWeight = FontWeight(800)
         )
         Row {
-            //images
-            //
+            Column {
+                Text(
+                    text = stringResource(id = R.string.YourChoice)
+                )
+                //image
+            }
+            Column {
+                Text(
+                    text = stringResource(id = R.string.opponentsChoice)
+                )
+                //image
+            }
         }
         Row {
             Button(onClick = { /*TODO*/ }) {
-                Text(text = "Rock")
+                Text(text = stringResource(id = R.string.rock))
             }
             Button(onClick = { /*TODO*/ }) {
-                Text(text = "Paper")
+                Text(text = stringResource(id = R.string.paper))
             }
             Button(onClick = { /*TODO*/ }) {
-                Text(text = "Scissors")
+                Text(text = stringResource(id = R.string.scissors))
             }
         }
     }
