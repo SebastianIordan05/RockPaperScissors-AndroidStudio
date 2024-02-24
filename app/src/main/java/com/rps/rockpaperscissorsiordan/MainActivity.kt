a1334c11913a54b161a046208191f15003a4dec3
SebastianIordan05 <sebastian.iordan@edu.iisleviponti.it>
2024-02-24 21:03:04 +0100

    added title, fun Layout() and modified fun GreetingPreview()

--- app/src/main/java/com/rps/rockpaperscissorsiordan/MainActivity.kt
+++ app/src/main/java/com/rps/rockpaperscissorsiordan/MainActivity.kt
@@ -3,13 +3,21 @@ package com.rps.rockpaperscissorsiordan
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rps.rockpaperscissorsiordan.ui.theme.RockPaperScissorsIordanTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +30,51 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Layout()
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "ciao $name!",
-        modifier = modifier
-    )
+fun Layout() {
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.padding(10.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.title)
+        )
+        Row {
+            //images
+            //
+        }
+        Row {
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Rock")
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Paper")
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Scissors")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     RockPaperScissorsIordanTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Layout()
+        }
     }
 }
