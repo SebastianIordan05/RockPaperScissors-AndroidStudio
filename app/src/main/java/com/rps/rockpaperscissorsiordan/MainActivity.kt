66b6439b244675d875ff845e116e4946355fac6d
SebastianIordan05 <sebastian.iordan@edu.iisleviponti.it>
2024-02-25 11:04:59 +0100

    added logic for both players

--- app/src/main/java/com/rps/rockpaperscissorsiordan/MainActivity.kt
+++ app/src/main/java/com/rps/rockpaperscissorsiordan/MainActivity.kt
@@ -3,34 +3,38 @@ package com.rps.rockpaperscissorsiordan
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.withConsumedWindowInsets
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import com.rps.rockpaperscissorsiordan.ui.theme.RockPaperScissorsIordanTheme
+import androidx.compose.foundation.layout.fillMaxWidth
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             RockPaperScissorsIordanTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -42,9 +46,11 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
 @Composable
 fun Layout() {
+    var selectedImg by remember { mutableStateOf("") }
+    var selectedImgOpponent by remember { mutableStateOf("") }
+
     Column (
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
@@ -54,31 +60,74 @@ fun Layout() {
             text = stringResource(id = R.string.title),
             fontWeight = FontWeight(800)
         )
+
         Row {
             Column {
                 Text(
                     text = stringResource(id = R.string.YourChoice)
                 )
-                //image
+                Image(
+                    painter = painterResource(id = getImageResourceId(selectedImg)),
+                    contentDescription = selectedImg,
+                    modifier = Modifier.size(100.dp)
+                )
             }
             Column {
                 Text(
                     text = stringResource(id = R.string.opponentsChoice)
                 )
-                //image
+                Image(
+                    painter = painterResource(id = getImageResourceId(selectedImgOpponent)),
+                    contentDescription = selectedImgOpponent,
+                    modifier = Modifier.size(100.dp)
+                )
             }
         }
-        Row {
-            Button(onClick = { /*TODO*/ }) {
+
+        Text(
+            text = stringResource(id = R.string.yourButtons)
+        )
+
+        Row (
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Button(onClick = { selectedImg = "rock" }) {
                 Text(text = stringResource(id = R.string.rock))
             }
-            Button(onClick = { /*TODO*/ }) {
+            Button(onClick = { selectedImg = "paper" }) {
                 Text(text = stringResource(id = R.string.paper))
             }
-            Button(onClick = { /*TODO*/ }) {
+            Button(onClick = { selectedImg = "scissors" }) {
                 Text(text = stringResource(id = R.string.scissors))
             }
         }
+
+        Text(
+            text = stringResource(id = R.string.opponentsButtons)
+        )
+
+        Row (
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Button(onClick = { selectedImgOpponent = "rock" }) {
+                Text(text = stringResource(id = R.string.rock))
+            }
+            Button(onClick = { selectedImgOpponent = "paper" }) {
+                Text(text = stringResource(id = R.string.paper))
+            }
+            Button(onClick = { selectedImgOpponent = "scissors" }) {
+                Text(text = stringResource(id = R.string.scissors))
+            }
+        }
+    }
+}
+
+fun getImageResourceId(game: String): Int {
+    return when (game) {
+        "rock" -> R.drawable.rock
+        "paper" -> R.drawable.paper
+        "scissors" -> R.drawable.scissors
+        else -> R.drawable.rock
     }
 }
 
@@ -87,7 +136,9 @@ fun Layout() {
 fun GreetingPreview() {
     RockPaperScissorsIordanTheme {
         Surface(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .fillMaxWidth(),
             color = MaterialTheme.colorScheme.background
         ) {
             Layout()
